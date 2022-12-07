c83a0c640eb1d9fa158f9571a28100caa2d30f2e
Simeon Kolev <simeonkolev99@gmail.com>
2022-12-07 21:02:31 +0100

    Added C# solution for Day1 problem.

---
+++ day1/c#/CalorieCounting/Program.cs
@@ -0,0 +1,14 @@
+﻿static int CalorieCount(int top)
+{
+    string inputPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "input.txt");
+
+    string text = File.ReadAllText(inputPath);
+    string[] splittedText = text.Split("\r\n\r\n");
+    IEnumerable<string[]> splittedArrays = splittedText.Select(s => s.Split("\r\n"));
+    IEnumerable<int> summedArrays = splittedArrays.Select((d) => d.Select(int.Parse).Sum());
+    IEnumerable<int> orderedArrays = summedArrays.OrderByDescending(num => num);
+
+    return orderedArrays.Take(top).Sum();
+}
+
+Console.WriteLine(CalorieCount(3));
